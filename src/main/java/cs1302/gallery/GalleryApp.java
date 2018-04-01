615ca520bfbc0b5e411e4289a20d0dbb89866d92
Ved Javle <vvjavle@gmail.com>
2018-04-01 16:01:02 -0400

    Organized imports and finished menu

--- src/main/java/cs1302/gallery/GalleryApp.java
+++ src/main/java/cs1302/gallery/GalleryApp.java
@@ -1,14 +1,34 @@
 package cs1302.gallery;
 
+//Image Imports
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+
+//Menu Imports
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem; 
+
+//Driver Class Imports
 import javafx.application.Application; 
+import javafx.stage.Stage;
+
+//Event Handler Imports
 import javafx.event.EventHandler;
 import javafx.scene.Scene; 
-import javafx.scene.input.MouseEvent; 
-import javafx.stage.Stage;
-import javafx.scene.layout.TilePane;
 
+//Layout Pane Imports
+import javafx.scene.layout.TilePane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+//Control Imports
+import javafx.scene.control.Button;
+
+
+//Miscellaneous Imports
+import javafx.scene.input.MouseEvent; 
 import java.awt.Insets;
 import java.io.File;
 import java.util.ArrayList;
@@ -19,58 +39,68 @@ public class GalleryApp extends Application
     @Override
     public void start(Stage stage) 
     {
-    	final double windowWidth = 640.0;
-    	final double windowHeight = 480.0;
-    	final int noOfHorrizontalImages = 3;
-    	final int noOfVerticalImages = 3;
-    	
-    	double imageViewWidth = windowWidth / noOfHorrizontalImages;
-    	double imageViewHeight = windowHeight  / noOfVerticalImages;
-    	
-    	TilePane pane = new TilePane();
-    	pane.setPrefColumns(noOfVerticalImages);
-
-		for(String s : getImageFilesInCurrentFolder())
-		{
-    		ImageView iv = new ImageView(new Image("File:" + s));
-    		iv.setFitHeight(imageViewWidth);
-    		iv.setFitWidth(imageViewHeight);
+    		final double windowWidth = 640.0;
+    		final double windowHeight = 480.0;
+//  		final int noOfHorrizontalImages = 5;
+//  		final int noOfVerticalImages = 1;
+//    	
+//    	double imageViewWidth = windowWidth / noOfHorrizontalImages;
+//    	double imageViewHeight = windowHeight  / noOfVerticalImages;
+//    	
+//    	TilePane pane = new TilePane();
+//    	pane.setPrefColumns(noOfVerticalImages);
+//
+//		for(String s : getImageFilesInCurrentFolder())
+//		{
+//    		ImageView iv = new ImageView(new Image("File:" + s));
+//    		iv.setFitHeight(imageViewWidth);
+//    		iv.setFitWidth(imageViewHeight);
+//
+//    		pane.getChildren().add(iv);
+//		}
+//		
+    		VBox topBar = new VBox();
     		
-    		EventHandler<MouseEvent> eventHandler = new EventHandler<MouseEvent>()
-    		{
-				@Override
-    			public void handle(MouseEvent mouseEvent)
-    			{
-    				System.out.println("Mouse clicked" + mouseEvent.getSource().toString());
-    			};
-    		};
-    		iv.addEventFilter(MouseEvent.MOUSE_CLICKED, eventHandler);
-
-    		pane.getChildren().add(iv);
-		}
+    		//File Menu
+    		Menu fileMenu = new Menu("File");
     		
-        Scene scene = new Scene(pane);
-
-        stage.setMaxWidth(windowWidth);
-        stage.setMaxHeight(windowHeight);
+    		//Menu Items
+    		MenuItem exit = new MenuItem("Exit");
+    		exit.setOnAction(e -> System.exit(0));
+    		fileMenu.getItems().add(exit);
+    		
+    		//Main Menu Bar
+    		MenuBar mainMenu = new MenuBar();
+    		mainMenu.getMenus().addAll(fileMenu);
+    		topBar.getChildren().add(mainMenu);
+    		
+    		HBox hbox = new HBox();
+    		
+    		//Button
+    		hbox.getChildren().add(new Button("Test1"));
+    		hbox.getChildren().add(new Button("Test2"));
+    		topBar.getChildren().add(hbox);
+    		
+		BorderPane borderPane = new BorderPane();
+		borderPane.setTop(topBar);
+		
+        Scene scene = new Scene(borderPane, windowWidth, windowHeight);
         stage.setTitle("[XYZ] Gallery!");
         stage.setScene(scene);
-        stage.sizeToScene();
-        stage.setResizable(false);
+        //stage.sizeToScene();
+        //stage.setResizable(false);
         stage.show();
     } // start
-
-
     
     private ArrayList<String> getImageFilesInCurrentFolder()
     {	
-    	ArrayList<String> fileList = new ArrayList();
+    		ArrayList<String> fileList = new ArrayList();
     	
-    	for(File file : new File(".").listFiles((dir, name) -> {return name.toLowerCase().endsWith(".jpg");}))
-    		if(file.isFile())
-    			fileList.add(file.getName());
+    		for(File file : new File(".").listFiles((dir, name) -> {return name.toLowerCase().endsWith(".jpg");}))
+    			if(file.isFile())
+    				fileList.add(file.getName());
     	
-    	return fileList;
+    		return fileList;
     }
 
 	public static void main(String[] args) 
