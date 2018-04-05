f10f63594edf140813eff7e14f63530a0fac639c
Ved Javle <vvjavle@gmail.com>
2018-04-04 22:24:30 -0400

    Added a simple event handler in FXML

---
+++ src/main/java/cs1302/gallery/GalleryAppController.java
@@ -0,0 +1,23 @@
+package cs1302.gallery;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class GalleryAppController extends Application 
+{
+    boolean isPlaying = false;
+    public void playButtonActionHandler(ActionEvent e)
+    {
+        ((Button) e.getSource()).setText((isPlaying = !isPlaying) ? "Pause" : "Play");
+    }
+
+    @Override
+    public void start(Stage arg0) throws Exception
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
